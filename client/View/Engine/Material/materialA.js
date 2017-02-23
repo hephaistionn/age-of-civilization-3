30f026c266d3e6fa2912c0eb614335b30dc87f24
hephaistionn <alexandre.monge.mail@gmail.com>
2017-02-23 01:44:24 +0100

    rename texture for entities

--- client/View/Engine/Material/materialA.js
+++ client/View/Engine/Material/materialA.js
@@ -1,6 +1,6 @@
 const THREE = require('three');
 const material = new THREE.MeshPhongMaterial({
-    map: THREE.ImageUtils.loadTexture('pic/home_00.png'),
+    map: THREE.ImageUtils.loadTexture('pic/entity.png'),
     side: THREE.DoubleSide
 });
 module.exports = material;
